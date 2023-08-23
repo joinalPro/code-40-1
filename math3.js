7b5322137c744c679f6008ed2ab2b38401c38593
Joinal Abden <joinalzx@gmail.com>
2023-08-23 13:25:31 +0600

    highest number

---
+++ math3.js
@@ -0,0 +1,16 @@
+// const jim = 84;
+// const dela = 99;
+// const chinku = 77;
+// const largest = (Math.max(jim, dela, chinku));
+//  console.log(largest);
+// if(jim > dela && jim > chinku){
+//     console.log('jim will get the cake');
+// } else if(dela >jim && dela > chinku ){
+//     console.log('dela will get the cake');
+// } else {
+//     console.log('chinku will get the cake')
+// }
+
+//using Function
+
+const maxNumber = [12, 21, 343];
